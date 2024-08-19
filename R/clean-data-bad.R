216acd83aed53fdde822947f1f9f18d97e2bf518
Kortney Ervin <kaervin@emory.edu>
2024-08-19 11:39:13 -0400

    added notes

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -4,6 +4,8 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 
 library(tidyverse)
 setwd("~/Documents/Teaching/Emory/epi590r-inclass/data/raw/")
+#wont work for me because it doesn't match the file structure I have
+#this is why we don't like using setwd
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -20,3 +22,6 @@ nlsy <- na.omit(nlsy)
 
 setwd("../clean/")
 write_rds(nlsy, "nlsy-complete-cases.rds")
+
+
+install.packages("here")
